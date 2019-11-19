29d263841b8a3f1b32a16001138c921ac705088c
asegesa12 <57462700+asegesa12@users.noreply.github.com>
2019-11-19 18:23:58 -0400

    Formularios

---
+++ Proyects/Proyects/Controllers/EventosAsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyects.Models;
+
+namespace Proyects.Controllers
+{
+    public class EventosAsController : Controller
+    {
+        private AgendaEntities1 db = new AgendaEntities1();
+
+        // GET: EventosAs
+        public ActionResult Index(string busqueda)
+        {
+            var lista = from x in db.EventosA
+                        select x;
+
+            if (String.IsNullOrEmpty(busqueda))
+            {
+                return View(db.EventosA.ToList());
+            }
+            else
+            {
+                lista = lista.Where(a => a.Eventos.Contains(busqueda));
+                return View(lista);
+            }
+        }
+
+        // GET: EventosAs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventosA eventosA = db.EventosA.Find(id);
+            if (eventosA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosA);
+        }
+
+        // GET: EventosAs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventosAs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Eventos,Fecha,Hora")] EventosA eventosA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EventosA.Add(eventosA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eventosA);
+        }
+
+        // GET: EventosAs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventosA eventosA = db.EventosA.Find(id);
+            if (eventosA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosA);
+        }
+
+        // POST: EventosAs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Eventos,Fecha,Hora")] EventosA eventosA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventosA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eventosA);
+        }
+
+        // GET: EventosAs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventosA eventosA = db.EventosA.Find(id);
+            if (eventosA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosA);
+        }
+
+        // POST: EventosAs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EventosA eventosA = db.EventosA.Find(id);
+            db.EventosA.Remove(eventosA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyects/Proyects/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyects.MvcApplication" Language="C#" %>
