84aa9861112314bcffefe4223ccbf1ba4f33ff8c
samuel encarnacion rosario <samuel20175369@hotmail.com>
2019-11-21 01:26:11 -1200

    cambio

---
+++ Proyects/Proyects/Controllers/AsignaturasController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyects.Models;
+
+namespace Proyects.Controllers
+{
+    public class AsignaturasController : Controller
+    {
+        private AgendaEntities2 db = new AgendaEntities2();
+
+        // GET: Asignaturas
+        public ActionResult Index(string busqueda)
+        {
+            var lista = from x in db.Asignatura
+                        select x;
+
+            if (String.IsNullOrEmpty(busqueda))
+            {
+                return View(db.Asignatura.ToList());
+            }
+            else
+            {
+                lista = lista.Where(a => a.Matricula.Contains(busqueda));
+                return View(lista);
+            }
+        }
+
+        // GET: Asignaturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asignaturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Matricula,Nombre,Asignatura1")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asignatura.Add(asignatura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+
+        // POST: Asignaturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Matricula,Nombre,Asignatura1")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asignatura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+
+        // POST: Asignaturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asignatura asignatura = db.Asignatura.Find(id);
+            db.Asignatura.Remove(asignatura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
