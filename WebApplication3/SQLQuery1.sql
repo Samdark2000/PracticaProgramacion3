e19fb739e0a0c699bca6968edce0436a46152785
Samdark2000 <55610239+Samdark2000@users.noreply.github.com>
2019-11-21 23:36:00 -0400

    Add files via upload

---
+++ WebApplication3/SQLQuery1.sql
@@ -0,0 +1,19 @@
+Create database Consulta
+Create database Agendact
+
+Create database Agenda3
+use Agenda3
+
+Create table Estudiantes(
+Id int Primary Key,
+Miembro varchar(20) not null ,
+Asignatura varchar(20) not null,
+Cuatrimestre int not null
+)
+
+Create table Agenda1(
+Nombre varchar (20)not null,
+Celular varchar (20)not null,
+Email  varchar (20)not null,
+Dirección varchar (20)not null
+)
